665a007a642cbbff37d3661259593655d3b67412
Elliot Sime <e.sime@lancs.ac.uk>
2015-02-03 18:11:43 +0000

    Fix comment

--- example_code/update.cpp
+++ example_code/update.cpp
@@ -11,7 +11,7 @@ void Update(int t)
   for (std::list<Entity>::iterator ent = entities.begin(); ent != entities.end(); ++ent) {
     ent->move(&(*ent));
     for (std::list<Entity>::iterator ent2 = ent; ent2 != entities.end(); ++ent2) //for every other element.
-      if (ent != ent2) // dont repeat
+      if (ent != ent2) // dont collide with self
         if (entity_CheckCollision(&(*ent), &(*ent2))) // if they are touching
           if (&(*ent) == player || ent2->entType == ENT_BULLET)  // is ent the player? or is ent2 a bullet?
             ent2->collide(&(*ent2), &(*ent));  // trigger collision on the bullet or the object hitting the player.
